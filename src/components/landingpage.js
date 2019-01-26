b2da544ed759714c431815f053ddb5e54c99c16c
naveendk <naveenkolambage@gmail.com>
2019-01-26 12:28:39 +1200

    added social links etc.

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -10,7 +10,30 @@ class Landing extends Component {
               <img
               src="./images/myAvatar.png"
               alt="avatar"
+              className="avatar-img"
               />
+
+              <div className="banner-text">
+                <h1>
+                    Full Stack Web Developer
+                </h1>
+                <hr></hr>
+                <p> HTML/CSS | Bootstrap | Wordpress | React | React Native | NodeJS | Express | MongoDB</p>
+              <div className="social-links">
+              {/*LinkedIN*/}
+                    <a href="https://www.linkedin.com/in/naveendk/" rel="noopener noreferrer" target="_blank">
+                    <i className ="fa fa-linkedin-square" aria-hidden="true"/>
+                    </a>  
+                {/*github*/}
+                     <a href="https://www.linkedin.com/in/naveendk/" rel="noopener noreferrer" target="_blank">
+                    <i className ="fa fa-github-square" aria-hidden="true"/>
+                    </a>    
+                      
+                    
+                    
+                    <a href="https://github.com/naveendk" ></a>           
+              </div>
+              </div>
               </Cell>
               </Grid>
             </div>
